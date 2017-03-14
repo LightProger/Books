bbf2e6f1a2313f1e3b1a567caae2c57528f1d6a6
Alexandr <photoluxe@mail.ru>
2017-03-14 23:46:02 +0600

    Создание объектов через массив - Книги

---
+++ src/head/first/java/BooksTestDrive.java
@@ -0,0 +1,44 @@
+package head.first.java;
+
+public class BooksTestDrive {
+
+     static class Books {
+        // Переменные
+        String title;
+        String author;
+
+        // Метод печати
+         public void print() {
+             System.out.print("Название книги: " + title);
+             System.out.println(", Автор: " + author);
+         }
+     }
+
+    public static void main(String[] args) {
+        // Создаем массив книг
+        Books[] myBooks = new Books[3];
+
+        // Создаем объекты - книгм
+        myBooks[0] = new Books();
+        myBooks[1] = new Books();
+        myBooks[2] = new Books();
+
+        // Описываем элементы массива
+        myBooks[0].title = "Плоды Java";
+        myBooks[1].title = "Java Гедсби";
+        myBooks[2].title = "Сборник рецептов по Java";
+
+        myBooks[0].author = "Боб";
+        myBooks[1].author = "Сью";
+        myBooks[2].author = "Ян";
+
+        // С помощью цикла передаем массив в метод печати
+        int x = 0;
+        while (x < 3) {
+            myBooks[x].print();
+            x += 1;
+        }
+    }
+}
+
+
